f95cbecf418c1cff87f564ba81741b54200c8c93
art07 <artlock1318@gmail.com>
2021-08-04 16:30:50 +0300

    some changes commit

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ import (
 
 // Main function
 func main() {
-	//fmt.Println("Test art branch....")
 	logger, _ := zap.NewProduction()
 	dsn := "postgres://postgres:secret@0.0.0.0:5432/test?sslmode=disable&timezone=utc"
 	db, err := sql.Open("pgx", dsn)
