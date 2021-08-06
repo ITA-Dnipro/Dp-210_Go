efeecce1686d67f3d1a3263e3184d709103284fd
glbter <nicknema13@gmail.com>
2021-08-06 16:41:35 +0300

    refactor(DPGO-16): rename to env, add conn url

--- config/env.go
+++ config/env.go
@@ -1,8 +1,14 @@
 package config
 
+import "net/url"
+
 type Env struct {
 	Connection string `env:"DB_CONNECTION" env-default:"postgres://postgres:secret@0.0.0.0:5432/test?sslmode=disable&timezone=utc"`
 	SqlDriver  string `env:"SQL_DRIVER" env-default:"pgx"`
 	Host       string `env:"HOST" env-default:"localhost"`
 	Port       string `env:"PORT" env-default:"8000"`
+}
+
+func (e *Env) ConnectionUrl() (*url.URL, error) {
+	return url.Parse(e.Connection)
 }
