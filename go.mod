401b20bd246bc542984ee15ccf742b5d6c814b80
Hlib Terentiev <54221016+glbter@users.noreply.github.com>
2021-08-11 18:26:40 +0300

    chore(DPGO-16): add config.json and .env, .json config struct (#10)
    the idea of this pr is to make .env and .json configs parsing easy to use by providing a structure and more reliable by default values
    
    * chore(DPGO-16): add db conn env config
    
    * refactor(DPGO-16): move out test data init code
    
    * refactor(DPGO-16): rename to env, add conn url
    
    * chore(DPGO-16): add config.json
    
    * refactor(DPGO-16): move test data, divide connUrl to params
    
    * typo(DPGO-16): connectionUrl to databaseUrl
    
    * chore(DPGO-16): remove test data
    
    * chore(DPGO-16): even env vars with art branch
    
    * fix: wrong db host
    
    * chore: run containers/ build  instruction

--- go.mod
+++ go.mod
@@ -10,8 +10,12 @@ require (
 	github.com/google/uuid v1.2.0
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/jackc/pgx/v4 v4.13.0
-	github.com/stretchr/testify v1.7.0
+
+	github.com/ilyakaznacheev/cleanenv v1.2.5 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+	github.com/jarcoal/httpmock v1.0.8
+	github.com/stretchr/testify v1.7.0 // indirect
+
 	go.uber.org/atomic v1.8.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.17.0
--- main.go
+++ main.go
@@ -1,77 +1,60 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/ITA-Dnipro/Dp-210_Go/internal/entity"
+	"github.com/ITA-Dnipro/Dp-210_Go/config"
 	"github.com/ITA-Dnipro/Dp-210_Go/internal/repository/postgres"
-	"github.com/ITA-Dnipro/Dp-210_Go/internal/repository/postgres/user"
-	"github.com/ITA-Dnipro/Dp-210_Go/internal/role"
 	router "github.com/ITA-Dnipro/Dp-210_Go/internal/server/http"
+
+	"github.com/ilyakaznacheev/cleanenv"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"go.uber.org/zap"
-	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	configPath     = "config.json"
+	migrationsPath = "migrations"
 )
 
 // Main function
 func main() {
-	fmt.Println("Start webapp dp210go OK!")
+	log.Println("Starting webapp dp210go")
+
+	var env config.Env
+	err := cleanenv.ReadEnv(&env)
+	if err != nil {
+		log.Fatal(fmt.Errorf("read env: %w", err))
+	}
+
+	var config config.Config
+	err = cleanenv.ReadConfig(configPath, &config)
+	if err != nil {
+		log.Fatal(fmt.Errorf("read config: %w", err))
+	}
+
 	logger, _ := zap.NewProduction()
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&timezone=utc",
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("POSTGRES_DB"))
-	db, err := sql.Open("pgx", dsn)
+
+	db, err := sql.Open("pgx", env.DatabaseStr())
+
 	if err != nil {
 		log.Fatal(fmt.Errorf("creating db: %w", err))
 	}
 	err = db.Ping()
 	if err != nil {
 		db.Close()
-		log.Fatal(fmt.Errorf("ping db %s : %w", dsn, err))
+		log.Fatal(fmt.Errorf("ping db %s : %w", env.DatabaseStr(), err))
 	}
 
-	migrationsPath := "migrations"
-	err = postgres.MigrateUp(migrationsPath, dsn)
+	err = postgres.MigrateUp(migrationsPath, env.DatabaseStr())
 	if err != nil {
 		log.Fatal(fmt.Errorf("db migrations: %w", err))
 	}
-	// TODO remove. for testing purpose.
-	repo := user.NewRepository(db)
-	hash, _ := bcrypt.GenerateFromPassword([]byte("admin"), bcrypt.MinCost)
-	repo.Create(context.Background(), entity.User{
-		ID:             "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
-		Name:           "admin",
-		Email:          "admin@admin.com",
-		PasswordHash:   hash,
-		PermissionRole: role.Admin,
-	})
-	hash, _ = bcrypt.GenerateFromPassword([]byte("operator"), bcrypt.MinCost)
-	repo.Create(context.Background(), entity.User{
-		ID:             "e4044a74-6557-4c3b-b2d8-4ef933430cf9",
-		Name:           "operator",
-		Email:          "operator@admin.com",
-		PasswordHash:   hash,
-		PermissionRole: role.Operator,
-	})
-	hash, _ = bcrypt.GenerateFromPassword([]byte("user"), bcrypt.MinCost)
-	repo.Create(context.Background(), entity.User{
-		ID:             "35ce783d-7f09-4ef1-bc27-8bddf1be24d3",
-		Name:           "test",
-		Email:          "test@admin.com",
-		PasswordHash:   hash,
-		PermissionRole: role.Viewer,
-	})
 
-	logger.Info("starting web server")
 	r := router.NewRouter(db, logger)
 	// Start server
-	log.Fatal(http.ListenAndServe("localhost:8000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v:%v", env.AppHost, env.AppPort), r))
 }
